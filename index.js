2dbd3eeb8fe763ec8866cc4ef36607f7c985b504
Dana Bradley <dbradley0909@outlook.com>
2023-01-22 23:57:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+// Strings
+let firstName= "Dana";
+let LastName= "Bradley";
+let first_Initial= "D";
+let last_Initial= "B";
+let bDay_Month= "September";  
+let bDay_Day= "nine";
+var favAnimals1= "dolphins";
+const favInsect2= "Spiders";
+const  favNumber= "nine";
+var numofChildren= "zero";
+
+//firstName = "dj";
+//console.log(firstName);
+//intergers
+let x = 129;    
+let y = 1239;   
+let d= x+y;
+let b= 15;
+var num=d+b;
+const num2= 22;
+const num3= 27;
+const numbers= num2+num3;
+var numbers2= numbers +numbers;
+var number3= numbers2+numbers2+ numbers;
+//console.log(number3);
+
+//array
+
+let danaNephews=["james","santana","jaceon","ziggy","jerzey"];
+let danaNiece=["caceon","saige","debby","semaji","tanaya"];
+const danaSisters=[0];
+const danaBrothers=["james","john","boomer"];
+var familybDay=["sept", "october","may", "june"];
+var parents=["mom","dad"];
+const favcars = ["honda", "chevy", "ford"];
+const favFood = ["cajun chicken pasta", "shrimp fired rice", "gumbo"];
+let favAlcohol = ["hand grenade", "daquiri", "armeretta pinapple"];
+let favdring = ["pink lemonade", "strawberry lemonade", "pinapple juice"];
+danaNephews[0];
+danaNiece[2];
+// console.log();
+
+//objects
+let cellphone={name:"lg", type:"smart phone", color: "silver"};
+let myself={name:"dana", bodyType:"obese", hairColor: "black"};
+let car={name:"honda", type:"accord", color: "blue", year: 2017};
+const niece = {firstName:"saige", age:14, eyeColor:"brown"};
+const boyfriend={firstName:"will", age:32, skinColor:"brown", hairColor:"black"};
+const mom={firstName:"antoinette", age:54, skinColor:"dark", hairColor:"black and grey"};
+const brother={firstName:"james", age:39, skinColor:"dark", hairColor:"black"};
+const rabbit = {name: "kelly",color: "brown",age: 3,goodGirl: true,};
+const {color: userColor ,make:userMake,model: userModel,year: userYear } = carDetails;
+let {title, make, model} = obj;
+console.log(car.name);
